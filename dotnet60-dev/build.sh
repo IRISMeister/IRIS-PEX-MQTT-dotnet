c78548223c53e2a53115cc5dda34ff5ace062e92
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2022-12-12 14:31:54 +0900

    minor fix

--- dotnet60-dev/build.sh
+++ dotnet60-dev/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cp -fR /source2/* .
 cp -fR lib mylib1/
-dotnet tool install --global Apache.Avro.Tools --version 1.11.0
 dotnet restore 
 dotnet publish -c debug -o /app
 echo "/app/myapp to run"
